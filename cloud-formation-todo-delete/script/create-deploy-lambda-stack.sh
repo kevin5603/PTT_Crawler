4ac8a1910a5be69a4313e1cc1f098e6a3f0ac665
kevin.lee <fly911338@gmail.com>
2024-05-15 01:33:14 +0800

    v3.11

--- cloud-formation-todo-delete/script/create-deploy-lambda-stack.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-aws cloudformation create-stack --stack-name ptt-crawler --template-body file://cloud-formation/create-lambda-cfn.yml --capabilities CAPABILITY_IAM --region us-west-2
--- cloud-formation-todo-delete/script/delete-deploy-lambda-stack.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-aws cloudformation delete-stack --stack-name ptt-crawler --region us-west-2
